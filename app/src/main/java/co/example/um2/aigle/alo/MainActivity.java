046ebb8bbbff7d09deb6a137b0d269990b91bffa
Mossi <mossi.hashemi@gmail.com>
2018-03-06 23:44:34 +0100

    first commit

---
+++ app/src/main/java/co/example/um2/aigle/alo/MainActivity.java
@@ -0,0 +1,16 @@
+package co.example.um2.aigle.alo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

49c0e4fb865357ac522a1fea9be87907cef91727
bennismehdi <34943435+bennismehdi@users.noreply.github.com>
2018-04-02 13:45:48 +0200

    Ajout de l'activity commerce et le onClick qui y conduit

---
+++ app/src/main/java/co/example/um2/aigle/alo/Common/Commerce/CommerceActivity.java
@@ -0,0 +1,15 @@
+package co.example.um2.aigle.alo.Common.Commerce;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import co.example.um2.aigle.alo.R;
+
+public class CommerceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_commerce);
+    }
+}
